cf25cd146ecdc2a8d2bc37ad75e20715f4d4ffa1
Barton <barton@192.168.1.100>
2020-12-01 16:30:24 +0800

    change ...

--- ngrokd-ubuntu.sh
+++ ngrokd-ubuntu.sh
@@ -53,9 +53,9 @@ install_ngrok() {
     echo '请输入解析的域名'
     read NGROK_DOMAIN
     openssl genrsa -out rootCA.key 2048
-    openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=whbaqn.com" -days 5000 -out rootCA.pem
+    openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
     openssl genrsa -out device.key 2048
-    openssl req -new -key device.key -subj "/CN=whbaqn.com" -out device.csr
+    openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
     openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
     
     cp rootCA.pem assets/client/tls/ngrokroot.crt
